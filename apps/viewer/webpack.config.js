c5ce942a04c7912952e16b88c2a1c973d6fc1a0e
Johnathan Hair <johnathan.hair@plantcom.com.au>
2019-01-12 11:11:35 +1000

    Swap to XeoGl for easier camera controls

--- apps/viewer/webpack.config.js
+++ apps/viewer/webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '.'),
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     rules: [
       {
